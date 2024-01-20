a9259bb3bd82b0ee108b5422e02c3c4016eb7dcc
Amanda <mandacosta94@gmail.com>
2024-01-19 21:53:30 -0300

    chore: PrismaORM inital configs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+const user = prisma.user.create({
+  data: {
+    name: 'Amanda Costa',
+    email: 'mandacosta94@gmail.com',
+  },
+})
