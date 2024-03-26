304b109a4f34dde632ce15a19a60877f3e3b3ae5
Amanda <mandacosta94@gmail.com>
2024-03-25 21:38:01 -0300

    feat: installing and configuring jwt and authenticated routes

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,15 @@
+import { makegetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send()
+  const userId = request.user.sub
+
+  const getUserProfileUseCase = makegetUserProfileUseCase()
+  const { user } = await getUserProfileUseCase.execute({
+    userId,
+  })
+  console.log(userId)
+  return reply.status(200).send({
+    user,
+  })
 }
